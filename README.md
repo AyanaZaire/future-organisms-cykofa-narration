7dbf44ab9b6303cdc59627171aa8db04298ec507
AyanaZaire <ayanazaire@gmail.com>
2024-09-02 09:04:57 -0400

    adding program and seed data

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# future-organisms-cykofa-narration
+# Cykofa Narration CLI
+The Cykofa Narration CLI (Command Line Interface) App is a tool for writing collectively authored fiction and poetry. The Cykofa Narration CLI is used as a teaching tool at [Seeda School](https://www.seedaschool.com/). If you would like to invite Seeda School to facilitate a Cykofa Narration Workshop please reach out via [email](mailto:info@seedaschool.com).
+
+## How To Get Started 
+1. Fork and clone this project.
+2. Open your terminal and run `$ json-server --watch db.json` in the project directory to start the server.
+2. In another terminal tab, run `$ node index.js` in the project directory to start the application on the command line.
+
+## What Is Cykofa?
+We know this place as the North Carolina Black River, they know it as Cykofa. A parallel universe suspended among past and future — where cornrows are cryptography keys, data farms are data forests, the weaving loom is a computer, a cloth is a document, and chain link fencing from demolished prisons are used as architectural membrane woven with plant life. In Cykofa the trees have learned to speak using the data Cykofians have encoded in the tree’s DNA and tree ring memory.
+
+## The Origin Story
+Remembering that we can store data into the DNA of plants and read information from a tree's rings through dendrochronology, I developed Cykofa Narration as a methodology. Cykofa Narration is a storytelling technology/methodology that relies on ecosystems reverence, collective voice, re-appropriation, and computer collaboration.  Developed and published during the [Ginkgo Bioworks Creative Residency](https://residency.ginkgobioworks.com/), [_Cykofa: The Seeda Origin Story_](https://ayanazairecotton.com/Cykofa-Story) is a long-form poetry narrative told through the journal entries of a non-binary biotechnologist named Seeda and the found data within an ancient, 2,600+ year-old bald cypress tree named Cy. Our narrator is a tree but also a portal, allowing us to traverse deep time and connections. The people of Cykofa have traditionally hosted their data within the DNA of their trees, but what happens when Seeda discovers a rip in the dendrochronological memory, exposing select datasets from our world? 
+
+Through foraging seed data related to biotechnology, poetry, abolition, southern self taught art and more—YouTube transcripts, found PDFs, website text, poems, journalism articles, etc.—are blended into a non-linear, choral hymn and Cy’s voice emerges. I have written a JavaScript program that splits paragraphs at punctuation marks such as periods and randomly recombines the paragraphs using a shuffling algorithm, resulting in Cy’s Narration and a woven world on the page. In many ways, [_Cykofa: The Seeda Origin Story_](https://ayanazairecotton.com/Cykofa-Story) was not written — it was grown.
+
+## What Is Cykofa Narration?
+Cykofa Narration is a story telling aesthetic that helps us write collectively authored fiction and poetry. This aesthetic allows us to explore non-linear storytelling and imagine new worlds using the data of our current one. Another world is possible. We may feel this by now, but how might we collectively imagine our way inside this unknowable future? By gathering found data — from poems, YouTube transcripts, love letters, twitter threads, zines, spells, reclaimed academic PDFs etc. — and algorithmically weaving the seemingly disparate text, we can begin to generate a collective voice as a prompt to tell a different story that troubles individual authorship and linear time.
+
+
+
---
+++ index.js
@@ -0,0 +1,222 @@
+const prompt = require('prompt');
+const fetch = require("node-fetch");
+
+let menu = {
+    1: "View Library",
+    2: "View Favorites",
+    3: "Add to Favorites",
+    4: "Edit Favorites",
+    5: "Generate Poem",
+    6: "Leave the Library"
+};
+
+function loadEntries() {
+  if (allEntries.length === 0) {
+    fetch("http://localhost:3000/entries")
+    .then(response => response.json())
+    .then(entries => {
+      entries.forEach(entry => {
+        allEntries.push(entry)
+      })
+    })
+  } else {
+    return allEntries
+  }
+}
+
+function entryIndex() {
+  allEntries.forEach(entry => {
+    console.log(entry.id, entry.name);
+  })
+  console.log("If you wish to view a single entry and/or add it to your favorites, please input the corresponding number below.");
+  prompt.start();
+  prompt.get(['entry'], function (err, result) {
+    console.log('Command-line input received:');
+    viewEntry(result.entry)
+  });
+}
+
+function viewEntry(id) {
+  // could filter through allEntries array to eliminate this call to database?
+  fetch(`http://localhost:3000/entries/${parseInt(id)}`)
+  .then(response => response.json())
+  .then(entry => {
+    console.log('\n' +
+      "Name:", entry.name + '\n' +
+      "Source Title:", entry.source_title + '\n' +
+      "Source Year:", entry.source_year + '\n' +
+      "Content:", entry.content + '\n');
+    console.log("Do you wish to add this entry to your Favorites? Please input yes or no below.");
+    prompt.start();
+    prompt.get(['input'], function (err, result) {
+      console.log('Yes/no input received:');
+      if (result.input == "yes") {
+        addFavorite(entry)
+      }
+      if (result.input == "no") {
+        launchLibrary()
+      }
+    });
+  })
+}
+
+function favoriteIndex() {
+  fetch("http://localhost:3000/favorites")
+  .then(response => response.json())
+  .then(favorites => {
+    if (favorites.length === 0) {
+      console.log("No favorites added yet. To add a favorite view entries below.");
+      entryIndex()
+    } else {
+      for (var i = 0; i < favorites.length; i++) {
+        favoritedEntries = allEntries.filter(entry => entry.id == favorites[i].entryId)
+        favoritedEntries.forEach(entry => {
+          console.log('\n' +
+            "Entry ID:", entry.id + '\n' +
+            "Name:", entry.name + '\n' +
+            "Content:", entry.content + '\n');
+        })
+      }
+      removeFavorite(favorites)
+    }
+  })
+}
+
+function removeFavorite(favorites) {
+  console.log("If you would like to remove an entry from your favorites, please input the corresponding Entry ID below. If you would like to return to main menu type, menu");
+  prompt.start();
+  prompt.get(['entry'], function (err, result) {
+    console.log('Command-line input received:', result.entry);
+    if (result.entry == "menu") {
+      launchLibrary()
+    } else {
+      let removedFavorites = favorites.filter(favorite => favorite.entryId == result.entry)
+      let removedFavorite = removedFavorites[0]
+      fetch(`http://localhost:3000/favorites/${removedFavorite.id}`, {
+        method: "DELETE",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(removedFavorite)
+      })
+      .then(response => response.json())
+      .then(deletedFavorite => {
+        console.log("Entry removed from favorites successfully! View current favorites below:");
+        favoriteIndex()
+      })
+    }
+  });
+}
+
+function addFavorite(entry) {
+  let favorite = {
+    entryId: entry.id,
+    profileId: 1
+  }
+  fetch("http://localhost:3000/favorites", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(favorite)
+  })
+  .then(response => response.json())
+  .then(favorite => {
+    console.log("Favorite added:", entry.name)
+    launchLibrary()
+  })
+}
+
+// IDEA: Generates a poem using the data's of your favorited entries
+function splitFavoriteBios() {
+  fetch("http://localhost:3000/favorites")
+  .then(response => response.json())
+  .then(favorites => {
+    if (favorites.length === 0) {
+      console.log("No favorites added yet. To add a favorite view entries below.");
+      entryIndex()
+    } else {
+      var singleArrayOfSentences = []
+      for (var i = 0; i < favorites.length; i++) {
+        favoritedEntries = allEntries.filter(entry => entry.id == favorites[i].entryId)
+        favoritedEntries.forEach(entry => {
+          console.log(entry.name, entry.id);
+          let splitBio = entry.content.split(".")
+          // push each sentence into a single array
+          for (let i = 0, len = splitBio.length; i < len ; i++) {
+            singleArrayOfSentences.push(splitBio[i])
+          }
+        })
+      }
+      generatePoem(singleArrayOfSentences)
+    }
+  })
+}
+
+// Fisher-Yates (aka Knuth) Shuffle.
+// https://stackoverflow.com/a/2450976
+function generatePoem(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  //log non-linear narrative
+  // console.log(JSON.stringify(array));
+  console.log(array);
+  array.forEach(e => console.log(e))
+}
+
+function launchLibrary() {
+  console.log("Welcome to the learning library!");
+  console.log("Input a number for one of the following menu options");
+  // create a printMenu() function instead?
+  console.log(menu);
+  // start the prompt
+  prompt.start()
+  // load the allEntries array (global variable)
+  loadEntries()
+  // display the main menu, asking the participant what they would like to do
+  prompt.get(['input'], function (err, result) {
+    console.log('Command-line input received.');
+    // View Library
+    if (result.input === "1") {
+      console.log('You chose: ' + menu[1]);
+      entryIndex()
+    }
+    // View Favorites
+    if (result.input === "2") {
+      console.log('You chose: ' + menu[2]);
+      favoriteIndex()
+    }
+    // Add to Favorites
+    if (result.input === "3") {
+      console.log('You chose: ' + menu[3]);
+      entryIndex()
+    }
+    // Edit Favorites
+    if (result.input === "4") {
+      console.log('You chose: ' + menu[4]);
+      favoriteIndex()
+    }
+    // Generate Poem
+    if (result.input === "5") {
+      console.log('You chose: ' + menu[5]);
+      splitFavoriteBios()
+    }
+    // Leave the Library
+    if (result.input === "6") {
+      console.log('You chose: ' + menu[6]);
+      console.log("You have left the library. Thank you for visiting! ")
+    }
+  })
+}
+
+var allEntries = []
+launchLibrary()
